f60c1a5a50f14575a81bd1c12fee81b3e7fadd21
anandvechalapu <anand.vechalapu@gmail.com>
2023-03-03 15:25:37 +0530

    initial commit

---
+++ Selecting the user stories0.7901430392089237.java
@@ -0,0 +1,24 @@
+
+Generating the Spring Boot Application
+
+To generate the Spring Boot application with controller, service and repository classes for the given user story, the following steps can be followed:
+
+1. Create a new project using the Spring Initializr.
+
+2. Select the language as Java and the Spring Boot version.
+
+3. Select the dependencies for web, JPA, and JIRA.
+
+4. Download the project and open it in an IDE like IntelliJ.
+
+5. Create a controller class and define the endpoint for the API to get projects information.
+
+6. Create a service class and define the method for calling the JIRA endpoint.
+
+7. Create a repository class and define the methods for querying the JIRA database.
+
+8. Create a test class and define the tests for verifying the system behaviour.
+
+9. Run the tests to check if the system is working as expected.
+
+10. Deploy the application to a web server.
